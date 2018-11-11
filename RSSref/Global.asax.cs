ac6c6da24a9437c9f3cd99ba76f721c681cd1b65
EvanBS <evandiesp@gmail.com>
2018-11-11 13:39:46 +0200

    roles created every time

--- RSSref/Global.asax.cs
+++ RSSref/Global.asax.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using RSSref.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -12,6 +15,10 @@ namespace RSSref
     {
         protected void Application_Start()
         {
+            SqlConnection.ClearAllPools();
+
+            Database.SetInitializer<ApplicationDbContext>(new AppDbInitializer());
+
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
