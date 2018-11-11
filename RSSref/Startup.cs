6cb1140a3c0aca2218eed357403fe11bc0c67b00
EvanBS <evandiesp@gmail.com>
2018-11-11 13:01:26 +0200

    Добавьте файлы проекта.

---
+++ RSSref/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RSSref.Startup))]
+namespace RSSref
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
